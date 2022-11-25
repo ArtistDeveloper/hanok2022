19eb3c2d1bb96b04e7cd7a510502a4f39055cfad
화이팅 <somadubel@gmail.com>
2022-11-26 02:14:00 +0900

    UPDATE: 벽 프리팹에 스프라이트 적용
    - Rotation변화
    - Position변화

--- hanok2022/Assets/Scripts/Wall.cs
+++ hanok2022/Assets/Scripts/Wall.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 // 벽에 그림자가 포함되어 있어야 한다.
 public class Wall : MonoBehaviour
 {
-    [SerializeField] Transform _shadowScale;
+    //[SerializeField] Transform _shadowScale;
     Vector3 _ceter = Vector3.zero;
 
     // TODO: 벽이 생성되면 플레이어 방향으로 이동하는 기능 구현 필요
     void Start()
     {
-        
+        MoveCenter();
     }
 
     void Update()
@@ -21,6 +22,6 @@ public class Wall : MonoBehaviour
 
     void MoveCenter()
     {
-
+        transform.DOMove(_ceter, 2);
     }
 }
