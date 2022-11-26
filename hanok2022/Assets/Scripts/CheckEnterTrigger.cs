d9b7feb80b78463ca4328e34c614f77e48db515a
qwo330 <qwd330@google.co.kr>
2022-11-27 00:33:45 +0900

    벽 데이터 처리 추가 (일단 방향만)

--- hanok2022/Assets/Scripts/CheckEnterTrigger.cs
+++ hanok2022/Assets/Scripts/CheckEnterTrigger.cs
@@ -4,16 +4,10 @@ public class CheckEnterTrigger : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.LogError("enter someing");
-        
         if (collision.CompareTag("Wall"))
         {
             var wall = collision.GetComponent<Wall>();
-
-            Destroy(wall.gameObject);
-
-            // todo : check wall data
-            Debug.LogError($"current dir: {GameManager.Instance.CurrentShadow.Direct}");
+            wall?.ValidateWallShadow();
         }
     }
 }
